42bddb84bfb549b2f841cee98ed6273b91184efd
Bikash Ranjan Bhoi <bhoi.bikash@gmail.com>
2020-03-10 22:18:30 +0530

    Update download_load.py

--- Session8/download_load.py
+++ Session8/download_load.py
@@ -28,8 +28,6 @@ def download_load():
 
 	#transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 	train_transforms = transforms.Compose([
-                                      #  transforms.Resize((28, 28)),
-                                      #  transforms.ColorJitter(brightness=0.10, contrast=0.1, saturation=0.10, hue=0.1),
                                        transforms.RandomRotation((-7.0, 7.0), fill=(1,)),
                                        transforms.ToTensor(),
                                        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) # The mean and std have to be sequences (e.g., tuples), therefore you should add a comma after the values. 
@@ -38,8 +36,6 @@ def download_load():
 
 	# Test Phase transformations
 	test_transforms = transforms.Compose([
-										  #  transforms.Resize((28, 28)),
-										  #  transforms.ColorJitter(brightness=0.10, contrast=0.1, saturation=0.10, hue=0.1),
 										   transforms.ToTensor(),
 										   transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 										   ])
