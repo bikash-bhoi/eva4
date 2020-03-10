018526e41b025f226f59a800e1b7af9aa2aeed38
Bikash Ranjan Bhoi <bhoi.bikash@gmail.com>
2020-03-10 18:04:50 +0530

    Update net.py

--- Session8/net.py
+++ Session8/net.py
@@ -94,7 +94,7 @@ class ResNet(nn.Module):
         out = F.avg_pool2d(out, 4)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
-        return out
+        return F.log_softmax(out)
 
 
 def ResNet18():
