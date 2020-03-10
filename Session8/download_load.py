60f20c42844906c9c6ba55aeaef9624cc6164e8c
Bikash Ranjan Bhoi <bhoi.bikash@gmail.com>
2020-03-10 22:14:01 +0530

    Update download_load.py

--- Session8/download_load.py
+++ Session8/download_load.py
@@ -26,12 +26,28 @@ def download_load():
 	if cuda:
 		torch.cuda.manual_seed(SEED)
 
-	transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+	#transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+	train_transforms = transforms.Compose([
+                                      #  transforms.Resize((28, 28)),
+                                      #  transforms.ColorJitter(brightness=0.10, contrast=0.1, saturation=0.10, hue=0.1),
+                                       transforms.RandomRotation((-7.0, 7.0), fill=(1,)),
+                                       transforms.ToTensor(),
+                                       transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) # The mean and std have to be sequences (e.g., tuples), therefore you should add a comma after the values. 
+                                       # Note the difference between (0.1307) and (0.1307,)
+                                       ])
+
+	# Test Phase transformations
+	test_transforms = transforms.Compose([
+										  #  transforms.Resize((28, 28)),
+										  #  transforms.ColorJitter(brightness=0.10, contrast=0.1, saturation=0.10, hue=0.1),
+										   transforms.ToTensor(),
+										   transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+										   ])
 
 	dataloader_args = dict(shuffle=True, batch_size=64, num_workers=4, pin_memory=True) if cuda else dict(shuffle=True, batch_size=64)
 
-	trainset = datasets.CIFAR10(root='./data', train=True,download=True, transform=transform)
-	testset = datasets.CIFAR10(root='./data', train=False,download=True, transform=transform)
+	trainset = datasets.CIFAR10(root='./data', train=True,download=True, transform=train_transforms)
+	testset = datasets.CIFAR10(root='./data', train=False,download=True, transform=test_transforms)
                                        
 
 	train_loader = torch.utils.data.DataLoader(trainset, **dataloader_args)
