fd3ce3c008bb64f047028b017f5e2b85c8c9cf8b
Bikash Ranjan Bhoi <bhoi.bikash@gmail.com>
2020-03-16 22:55:58 +0530

    Update resnet.py

--- Session9/models/resnet.py
+++ Session9/models/resnet.py
@@ -9,11 +9,11 @@ class BasicBlock(nn.Module):
     def __init__(self, in_planes, planes, stride=1):
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-	self.drop0 = nn.Dropout2d(0.15)
+    self.drop0 = nn.Dropout2d(0.15)
         self.bn1 = nn.BatchNorm2d(planes)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
-	self.drop1 = nn.Dropout2d(0.15)
+    self.drop1 = nn.Dropout2d(0.15)
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion*planes:
@@ -64,11 +64,11 @@ class ResNet(nn.Module):
         self.in_planes = 64
 
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
-		self.Drop0=nn.Dropout2d(0.15)
-		self.Drop1=nn.Dropout2d(0.15)
-		self.Drop2=nn.Dropout2d(0.15)
-		self.Drop3=nn.Dropout2d(0.15)
-		self.Drop4=nn.Dropout2d(0.15)
+        self.Drop0=nn.Dropout2d(0.15)
+        self.Drop1=nn.Dropout2d(0.15)
+        self.Drop2=nn.Dropout2d(0.15)
+        self.Drop3=nn.Dropout2d(0.15)
+        self.Drop4=nn.Dropout2d(0.15)
         self.bn1 = nn.BatchNorm2d(64)
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
