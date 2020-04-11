937d0dc7c8d8b608b5ed342c75dc66df436dc3cb
Bikash Ranjan Bhoi <bhoi.bikash@gmail.com>
2020-04-11 19:26:53 +0530

    Update zig_zag_plot.py

--- utils/zig_zag_plot.py
+++ utils/zig_zag_plot.py
@@ -2,7 +2,7 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 
-def zigzag_plot(num_trianges,min_val,max_val,triangle_base):
+def zigzag_plot(num_trianges = 3, min_val = 0, max_val = 1, triangle_base = 2):
     x = np.linspace(0, num_trianges * triangle_base, num=1000)
     tan_val = 2*max_val/triangle_base
     print(tan_val)
