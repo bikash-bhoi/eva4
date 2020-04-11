95945180aba886d97fdddca0e6c1c4857a538680
Bikash Ranjan Bhoi <bhoi.bikash@gmail.com>
2020-04-11 13:26:04 +0530

    Update data_albument.py

--- utils/data_albument.py
+++ utils/data_albument.py
@@ -8,14 +8,14 @@ class TrainAlbumentation():
 	def __init__(self):
 		self.train_trans = Compose([
 		HorizontalFlip(),
-		#Rotate((-10.0, 10.0)),
-		#Normalize(
-		#	mean=[0.485,0.456,0.406],
-		#	std=[0.229,0.224,0.225],
-		#),
+		Rotate((-10.0, 10.0)),
+		Normalize(
+			mean=[0.485,0.456,0.406],
+			std=[0.229,0.224,0.225],
+		),
 		PadIfNeeded(min_height=40, min_width=40, border_mode=4, always_apply=False,value=0, p=1.0),
-		#RandomCrop(height=32, width=32, always_apply=True, p=1.0, padding=4),
-		#Cutout(num_holes=8, max_h_size=8, max_w_size=8, always_apply=False, p=0.5),
+		RandomCrop(height=32, width=32, always_apply=True, p=1.0),
+		Cutout(num_holes=8, max_h_size=8, max_w_size=8, always_apply=False, p=0.5),
 		ToTensor()
 		])
 
