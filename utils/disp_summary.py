48aec93106589b64429cc8f8390af5ef0fd9960e
Bikash Ranjan Bhoi <bhoi.bikash@gmail.com>
2020-04-18 18:10:55 +0530

    Update disp_summary.py

--- utils/disp_summary.py
+++ utils/disp_summary.py
@@ -2,8 +2,8 @@ import torchvision
 import torchsummary
 from torchsummary import summary
 
-def disp_summary(model):
+def disp_summary(model,img_size = [3,32,32]):
 	#use_cuda= torch.cuda.is_available()
 	#device=torch.device('cuda' if use_cuda else 'cpu')
 	##model=Net().to(device)
-	summary(model, input_size=(3,32,32))
+	summary(model, input_size=(img_size[0],img_size[1],img_size[2]))
