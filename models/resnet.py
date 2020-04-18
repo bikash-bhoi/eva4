5a781ca99e1fc89a8bcdf2a72b3cf442f1d15e1c
Bikash Ranjan Bhoi <bhoi.bikash@gmail.com>
2020-04-18 18:19:42 +0530

    Update resnet.py

--- models/resnet.py
+++ models/resnet.py
@@ -96,11 +96,11 @@ class ResNet(nn.Module):
 		return F.log_softmax(out)
 
 
-def ResNet18():
-	return ResNet(BasicBlock, [2,2,2,2])
+def ResNet18(num_classes=10):
+	return ResNet(BasicBlock, [2,2,2,2],num_classes=10)
 
-def ResNet34():
-	return ResNet(BasicBlock, [3,4,6,3])
+def ResNet34(num_classes=10):
+	return ResNet(BasicBlock, [3,4,6,3],num_classes=10)
 
 def ResNet50():
 	return ResNet(Bottleneck, [3,4,6,3])
