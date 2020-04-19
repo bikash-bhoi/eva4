da8e4fac79732ab5b3ad39407e06984d30991c0d
Bikash Ranjan Bhoi <bhoi.bikash@gmail.com>
2020-04-20 00:21:21 +0530

    Update train_test.py

--- utils/train_test.py
+++ utils/train_test.py
@@ -67,6 +67,7 @@ def test(model, device, test_loader):
 	model.eval()
 	test_loss = 0
 	correct = 0
+	processed = 0
 	
 	with torch.no_grad():
 		for data, target in test_loader:
@@ -77,15 +78,16 @@ def test(model, device, test_loader):
 			test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
 			pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
 			correct += pred.eq(target.view_as(pred)).sum().item()
+			processed += len(data)
 
 	test_loss /= len(test_loader.dataset)
 	test_losses_l1.append(test_loss)
 
 	print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
-		test_loss, correct, len(test_loader.dataset),
-		100. * correct / len(test_loader.dataset)))
+		test_loss, correct, processed,
+		100. * correct / processed))
 	
-	test_acc_l1.append(100. * correct / len(test_loader.dataset))
+	test_acc_l1.append(100. * correct / processed)
 	return test_acc_l1[-1]
 
 def fit(model, device, train_loader, test_loader, optimizer, scheduler, num_epoch):
